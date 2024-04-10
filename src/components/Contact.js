f134d9e94639fc8a7291bee4a4ae319e99804e9f
“CS21B029” <cs21b029@iittp.ac.in>
2024-04-10 08:38:55 +0530

    full

---
+++ src/components/Contact.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import '../styles/Contact.css'
+const Contact = () => {
+  return (
+    <div className="details">
+        <div className='contact'>
+      <h1>My Contact Details</h1>
+      <div className="mail">
+        <h3>Email Id:</h3>
+        <p>cs21b029@iittp.ac.in</p>
+      </div>
+      <div className="phone">
+        <h3>Moblie Number:</h3>
+        <p>+91 9704141029</p>
+      </div>  
+    </div>
+    </div>
+  )
+}
+
+export default Contact
